f8cfbf098b0c4be40bbab16df759b8127985c712
martinfjant <martin@falkjohansson.se>
2018-04-13 14:12:24 +0200

    Added ESlint configured for react and flow

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-//@flow
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { AUTH } from './Redux/Actions/Sessions/Types'
-import reducers from './Redux/Reducers'
-import Routes from './Components/Routes'
-import registerServiceWorker from './registerServiceWorker'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './Assets/Styles/Style.css'
-
-const composeEnhancers = initReduxDevTools()
-const appReducer = combineReducers(reducers)
+// @flow
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { AUTH } from './Redux/Actions/Sessions/Types';
+import reducers from './Redux/Reducers';
+import Routes from './Components/Routes';
+import registerServiceWorker from './registerServiceWorker';
+import './Assets/Styles/Style.css';
+// eslint-disable-next-line no-use-before-define
+const composeEnhancers = initReduxDevTools();
+const appReducer = combineReducers(reducers);
 const rootReducer = (state, action) => {
-  if (action.type === AUTH)
-    state = undefined
+  if (action.type === AUTH) { state = undefined; }
 
-  return appReducer(state, action)
-}
-const middleware = typeof(composeEnhancers) === 'function' ? composeEnhancers(applyMiddleware(thunk)) : applyMiddleware(thunk)
-const store = createStore(rootReducer, middleware)
-
+  return appReducer(state, action);
+};
+const middleware = typeof (composeEnhancers) === 'function' ? composeEnhancers(applyMiddleware(thunk)) : applyMiddleware(thunk);
+const store = createStore(rootReducer, middleware);
+const app: any = document.getElementById('app');
 ReactDOM.render(
   <Provider store={store}>
     <Routes />
   </Provider>,
-  document.getElementById('app')
-)
+  app,
+);
 
-registerServiceWorker()
+registerServiceWorker();
 
 function initReduxDevTools() {
-  return process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null
+  // eslint-disable-next-line
+  return process.env.NODE_ENV === 'development' ? window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ : null;
 }
