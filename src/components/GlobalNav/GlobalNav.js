42c8bb4fe0aad12686cef4ce035f1a7adeebb677
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-04-19 22:48:43 +0200

    fixing imports

--- src/components/GlobalNav/GlobalNav.js
+++ src/components/GlobalNav/GlobalNav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Link } from "react-router";
-import { Button } from "../Button";
+import { Link } from "react-router-dom";
+import { Button } from "..";
 
 const GlobalNav = () => (
   <nav className="navbarMenu">
