691152dfda74a07c539b5376bb2ebc806da2003e
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-04-19 22:42:57 +0200

    exported in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as Input } from './Input/Input';
-export { default as Button } from './Button/Button';
-export { default as Card } from './Card/Card';
-export { default as App } from './App/App';
-export { default as LinkList } from './LinkList/LinkList';
+export { default as Input } from "./Input/Input";
+export { default as Button } from "./Button/Button";
+export { default as Card } from "./Card/Card";
+export { default as App } from "./App/App";
+export { default as LinkList } from "./LinkList/LinkList";
+export { default as GlobalNav } from "./GlobalNav/GlobalNav";
