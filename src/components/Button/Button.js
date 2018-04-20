3d9a742e1055c5ca6abd496baa6f9bca939c7476
Anna Ross <anna.ross@chasacademy.se>
2018-04-20 17:00:32 +0200

    Create an Icon component that renders svg

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 import './Button.css';
+/* TODO: incorporate a way to add an icon to <Button />
+         add props validation
+         figure out/refactor implementation of label & title
+*/
 
-const Button = ({ color = 'primary', ...props }) => (
-  <button className={`button ${color}`} {...props} />
+const Button = ({
+  color = 'primary',
+  label = null,
+  children,
+  ...props
+}) => (
+  <button className={`button ${color}`} {...props} label={label}>
+    {children}
+  </button>
 );
 
 export default Button;
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,36 +3,53 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs/react";
-import { withNotes } from '@storybook/addon-notes';
+import { withNotes } from "@storybook/addon-notes";
 
 import withTests from "./withTests";
 import "../assets/styles/style.css";
 
-import { Button, Input, Progress, GlobalNav, LinkList, Item } from "../components";
+import {
+  Button,
+  Input,
+  Progress,
+  GlobalNav,
+  LinkList,
+  Item,
+  Icon
+} from "../components";
 
 /***** Stories of Button component *****/
 action("trigger-an-event-perhaps");
 
-const knobStoriesButton = storiesOf("Elements/Button", module);
-knobStoriesButton.addDecorator(withKnobs);
+const buttonStories = storiesOf("Elements/Button", module);
+buttonStories.addDecorator(withKnobs);
 
-knobStoriesButton.add("disable/enable", () => (
-  <Button color="primary" disabled={boolean("Disabled", true)}>
-    {text("Label", "Write what you want!")}
-  </Button>
-));
+buttonStories
+  .add("disable/enable", () => (
+    <Button color="primary" disabled={boolean("Disabled", true)}>
+      {text("Label", "Write what you want!")}
+    </Button>
+  ))
+  .add(
+    "primary",
+    withNotes("'Cool' is defined by it's label attribute")(() => (
+      <Button color="primary" label="Cool" />
+    ))
+  )
+  .add(
+    "secondary",
+    withNotes("No label attr or children provided")(() => (
+      <Button color="secondary" />
+    ))
+  )
+  .add("info", () => <Button color="info" label="some aria-label" />)
+  .add("warning", () => <Button color="warning" label="warning" />)
 
-storiesOf("Elements/Button", module)
-  .add("default",  withNotes('A very simple component')(() => <Button color="info">Info</Button>))
-  .add("primary", () => <Button color="primary">Primary</Button>)
-  .add("hovered", () => <Button color="secondary">Secondary</Button>)
-  .add("active", () => <Button color="default">Default</Button>);
-
-storiesOf("Elements/Button/with actions", module).add("simple trigger", () => (
-  <Button color="primary" onClick={action("trigger-an-event-perhaps")}>
-    Click me!
-  </Button>
-));
+  .add("trigger action", () => (
+    <Button color="secondary" onClick={action("trigger-an-action-perhaps")}>
+      Click me!
+    </Button>
+  ));
 
 /***** Stories of Input component *****/
 storiesOf("Elements/Input", module)
@@ -42,8 +59,9 @@ storiesOf("Elements/Input", module)
   .add("radio", () => <Input type="radio" />)
   .add("checkbox", () => <Input type="checkbox" />);
 
-  storiesOf("Elements/Item", module)
-  .add("text", () => <Item ItemId="1" ItemTitle="Titel" />)
+storiesOf("Elements/Item", module).add("text", () => (
+  <Item ItemId="1" ItemTitle="Titel" />
+));
 
 /***** Stories of Progress component *****/
 
@@ -52,6 +70,15 @@ progressStories.addDecorator(withKnobs);
 
 progressStories
   .add("0", () => <Progress />)
-  .add("30", () => <Progress value="30"/>)
+  .add("30", () => <Progress value="30" />)
   .add("60", () => <Progress value="60" />)
-  .add("100", () => <Progress value="100"/>)
+  .add("100", () => <Progress value="100" />);
+
+/***** Stories of Icon component *****/
+storiesOf("Elements/Icon", module)
+  .add("home", () => <Icon icon="home" color="lavender" />)
+  .add("plus", () => <Icon icon="plus" color="pink" />)
+  .add("cog", () => <Icon icon="cog" color="orange" transform="scale(1)" />)
+  .add("rocket", () => (
+    <Icon icon="rocket" color="aquamarine" width="30" transform="scale(1)" />
+  ));
