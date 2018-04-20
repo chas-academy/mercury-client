ee503f6cc0fae799cb3e0357da037261168aab22
Anna Ross <anna.ross@chasacademy.se>
2018-04-20 16:52:07 +0200

    Rearrange location of navbar - imported in App instead of Home view

--- src/components/GlobalNav/GlobalNav.js
+++ src/components/GlobalNav/GlobalNav.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "..";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '..';
 
 const GlobalNav = () => (
   <nav className="navbarMenu">
