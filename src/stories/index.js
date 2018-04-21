e0e10332b06a690179622fce0a5de1584304e559
Anna Ross <anna.ross@chasacademy.se>
2018-04-21 20:03:27 +0200

    Add sizing option and set width/height at <path> instead of <svg> for correct scaling

--- src/stories/index.js
+++ src/stories/index.js
@@ -43,6 +43,7 @@ buttonStories
     ))
   )
   .add("info", () => <Button color="info" label="some aria-label" />)
+<<<<<<< d0fa2c086d9b9a438aa3ed873e1e9b95cc96a6e5
   .add("warning", () => <Button color="warning" label="warning" />)
 
   .add("trigger action", () => (
@@ -50,6 +51,30 @@ buttonStories
       Click me!
     </Button>
   ));
+=======
+  .add("warning", () => (
+    <Button color="light">
+      <Icon icon="rocket" color="aquamarine" />
+    </Button>
+  ));
+
+storiesOf("Elements/Button", module)
+  .add(
+    "default",
+    withNotes("A very simple component")(() => (
+      <Button color="info">Info</Button>
+    ))
+  )
+  .add("cool", () => <Button color="primary">Primary</Button>)
+  .add("hovered", () => <Button color="secondary">Secondary</Button>)
+  .add("active", () => <Button color="default">Default</Button>);
+
+storiesOf("Elements/Button/with actions", module).add("simple trigger", () => (
+  <Button color="primary" onClick={action("trigger-an-event-perhaps")}>
+    Click me!
+  </Button>
+));
+>>>>>>> Add sizing option and set width/height at <path> instead of <svg> for correct scaling
 
 /***** Stories of Input component *****/
 storiesOf("Elements/Input", module)
@@ -59,16 +84,20 @@ storiesOf("Elements/Input", module)
   .add("radio", () => <Input type="radio" />)
   .add("checkbox", () => <Input type="checkbox" />);
 
+<<<<<<< d0fa2c086d9b9a438aa3ed873e1e9b95cc96a6e5
 storiesOf("Elements/Item", module).add("text", () => (
   <Item ItemId="1" ItemTitle="Titel" />
 ));
 
+=======
+>>>>>>> Add sizing option and set width/height at <path> instead of <svg> for correct scaling
 /***** Stories of Progress component *****/
 
 const progressStories = storiesOf("Elements/Progress", module);
 progressStories.addDecorator(withKnobs);
 
 progressStories
+<<<<<<< d0fa2c086d9b9a438aa3ed873e1e9b95cc96a6e5
   .add("0", () => <Progress />)
   .add("30", () => <Progress value="30" />)
   .add("60", () => <Progress value="60" />)
@@ -82,3 +111,17 @@ storiesOf("Elements/Icon", module)
   .add("rocket", () => (
     <Icon icon="rocket" color="aquamarine" width="30" transform="scale(1)" />
   ));
+=======
+  .add("0%", () => <Progress />)
+  .add("30%", () => <Progress value="30" />)
+  .add("60%", () => <Progress value="60" />)
+  .add("100%", () => <Progress value="100" />);
+
+storiesOf("Elements/Icon", module)
+  .add("home, default size - small", () => <Icon icon="home" color="#5e515a" />)
+  .add("plus - medium", () => (
+    <Icon icon="plus" color="#d9b1b1" size="medium" />
+  ))
+  .add("cog - large", () => <Icon icon="cog" color="orange" size="large" />)
+  .add("rocket, no color provided", () => <Icon icon="rocket" size="large" />);
+>>>>>>> Add sizing option and set width/height at <path> instead of <svg> for correct scaling
