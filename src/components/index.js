4e38861761c7b18d962bf492027a05b0c18930a0
Anna Ross <anna.ross@chasacademy.se>
2018-04-23 15:28:36 +0200

    Change name of props into camelCase

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
+export { default as App } from './App/App';
 export { default as Input } from './Input/Input';
 export { default as Button } from './Button/Button';
 export { default as Card } from './Card/Card';
-export { default as App } from './App/App';
 export { default as LinkList } from './LinkList/LinkList';
 export { default as GlobalNav } from './GlobalNav/GlobalNav';
 export { default as ProgressBar } from './ProgressBar/ProgressBar';
 export { default as Item } from './Item/Item';
+export { default as ItemList } from './Item/Item';
 export { default as Icon } from './Icon/Icon';
 export { default as DummyData } from '../DummyData';
