5709e8911edd478c41d0c1edaf710d6844ba7ee0
martinfjant <martin@falkjohansson.se>
2018-04-23 16:19:12 +0200

    Added custom Item type

---
+++ src/types.js
@@ -0,0 +1,16 @@
+// @flow
+
+export type Item = {
+    id: number,
+    name: string,
+    price: number,
+    delimiter: number,
+    created: Date,
+    updated: Date
+}
+
+export type User = {
+    id: number,
+    name: string
+}
+
