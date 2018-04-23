b60f4e10a9392c4d3709d4a1e98c69378fa1acfc
Anna Ross <anna.ross@chasacademy.se>
2018-04-23 13:32:29 +0200

    Rebasing local branch onto dev

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,10 @@
 import React from 'react';
 
 import './Button.css';
-/* TODO: incorporate a way to add an icon to <Button />
-         add props validation
-         figure out/refactor implementation of label & title
-*/
+/* TODO: add props validation */
 
-const Button = ({
-  color = 'primary',
-  label = null,
-  children,
-  ...props
-}) => (
-  <button className={`button ${color}`} {...props} label={label}>
+const Button = ({ color = 'default', children, ...props }) => (
+  <button className={`button ${color}`} {...props}>
     {children}
   </button>
 );
