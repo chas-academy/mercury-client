1c21fc829e699cb6b61e0380fbc84185585845c5
Anna Ross <anna.ross@chasacademy.se>
2018-04-23 13:32:29 +0200

    Rebasing local branch onto dev

--- src/stories/index.js
+++ src/stories/index.js
@@ -19,62 +19,45 @@ import {
 } from "../components";
 
 /***** Stories of Button component *****/
-action("trigger-an-event-perhaps");
 
 const buttonStories = storiesOf("Elements/Button", module);
 buttonStories.addDecorator(withKnobs);
+action("trigger-an-event-perhaps");
 
 buttonStories
+  .add(
+    "No props is passed",
+    withNotes("No props is passed")(() => <Button>button</Button>)
+  )
+  .add(
+    "No children",
+    withNotes(
+      "No children provided, only passing color = secondary, which adds the class secondary"
+    )(() => <Button color="secondary" onClick={action("trigger-an-event")} />)
+  )
+  .add("button w. icon", () => (
+    <Button onClick={action("trigger-an-event")}>
+      <Icon icon="rocket" color="pink" />
+    </Button>
+  ))
   .add("disable/enable", () => (
-    <Button color="primary" disabled={boolean("Disabled", true)}>
+    <Button
+      color="primary"
+      disabled={boolean("Disabled", true)}
+      onClick={action("trigger-an-event")}
+    >
       {text("Label", "Write what you want!")}
     </Button>
   ))
   .add(
-    "primary",
-    withNotes("'Cool' is defined by it's label attribute")(() => (
-      <Button color="primary" label="Cool" />
+    "w. icon and text",
+    withNotes("")(() => (
+      <Button color="secondary" onClick={action("trigger-an-event")}>
+        <Icon icon="rocket" color="var(--light)" />
+        <span>rocket</span>
+      </Button>
     ))
-  )
-  .add(
-    "secondary",
-    withNotes("No label attr or children provided")(() => (
-      <Button color="secondary" />
-    ))
-  )
-  .add("info", () => <Button color="info" label="some aria-label" />)
-<<<<<<< d0fa2c086d9b9a438aa3ed873e1e9b95cc96a6e5
-  .add("warning", () => <Button color="warning" label="warning" />)
-
-  .add("trigger action", () => (
-    <Button color="secondary" onClick={action("trigger-an-action-perhaps")}>
-      Click me!
-    </Button>
-  ));
-=======
-  .add("warning", () => (
-    <Button color="light">
-      <Icon icon="rocket" color="aquamarine" />
-    </Button>
-  ));
-
-storiesOf("Elements/Button", module)
-  .add(
-    "default",
-    withNotes("A very simple component")(() => (
-      <Button color="info">Info</Button>
-    ))
-  )
-  .add("cool", () => <Button color="primary">Primary</Button>)
-  .add("hovered", () => <Button color="secondary">Secondary</Button>)
-  .add("active", () => <Button color="default">Default</Button>);
-
-storiesOf("Elements/Button/with actions", module).add("simple trigger", () => (
-  <Button color="primary" onClick={action("trigger-an-event-perhaps")}>
-    Click me!
-  </Button>
-));
->>>>>>> Add sizing option and set width/height at <path> instead of <svg> for correct scaling
+  );
 
 /***** Stories of Input component *****/
 storiesOf("Elements/Input", module)
@@ -84,34 +67,16 @@ storiesOf("Elements/Input", module)
   .add("radio", () => <Input type="radio" />)
   .add("checkbox", () => <Input type="checkbox" />);
 
-<<<<<<< d0fa2c086d9b9a438aa3ed873e1e9b95cc96a6e5
 storiesOf("Elements/Item", module).add("text", () => (
   <Item ItemId="1" ItemTitle="Titel" />
 ));
 
-=======
->>>>>>> Add sizing option and set width/height at <path> instead of <svg> for correct scaling
 /***** Stories of Progress component *****/
 
 const progressStories = storiesOf("Elements/Progress", module);
 progressStories.addDecorator(withKnobs);
 
 progressStories
-<<<<<<< d0fa2c086d9b9a438aa3ed873e1e9b95cc96a6e5
-  .add("0", () => <Progress />)
-  .add("30", () => <Progress value="30" />)
-  .add("60", () => <Progress value="60" />)
-  .add("100", () => <Progress value="100" />);
-
-/***** Stories of Icon component *****/
-storiesOf("Elements/Icon", module)
-  .add("home", () => <Icon icon="home" color="lavender" />)
-  .add("plus", () => <Icon icon="plus" color="pink" />)
-  .add("cog", () => <Icon icon="cog" color="orange" transform="scale(1)" />)
-  .add("rocket", () => (
-    <Icon icon="rocket" color="aquamarine" width="30" transform="scale(1)" />
-  ));
-=======
   .add("0%", () => <Progress />)
   .add("30%", () => <Progress value="30" />)
   .add("60%", () => <Progress value="60" />)
@@ -123,5 +88,4 @@ storiesOf("Elements/Icon", module)
     <Icon icon="plus" color="#d9b1b1" size="medium" />
   ))
   .add("cog - large", () => <Icon icon="cog" color="orange" size="large" />)
-  .add("rocket, no color provided", () => <Icon icon="rocket" size="large" />);
->>>>>>> Add sizing option and set width/height at <path> instead of <svg> for correct scaling
+  .add("rocket, no color provided", () => <Icon icon="rocket" size="xlarge" />);
