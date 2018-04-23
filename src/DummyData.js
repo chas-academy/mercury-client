c219abea3348df2fe8c9daa5e3e02fcd80ae063b
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-04-23 09:31:27 +0200

    Dummy data now created.

---
+++ src/DummyData.js
@@ -0,0 +1,36 @@
+const DummyData = [
+  {
+    ItemId: '01',
+    ItemTitle: 'iphone X',
+    Goal: '100',
+    Usage: '300 ggr',
+    PurchasePrice: '12490 kr',
+    PurchaseDate: '17-11-19',
+  },
+  {
+    ItemId: '02',
+    ItemTitle: 'Bose quietcomfort 35 ii',
+    Goal: '20',
+    Usage: '67 ggr',
+    PurchasePrice: '3990 kr',
+    PurchaseDate: '18-03-01',
+  },
+  {
+    ItemId: '03',
+    ItemTitle: 'LOUIS VUITTON Belmont Damier Ebene Canvas',
+    Goal: '50',
+    Usage: '38 ggr',
+    PurchasePrice: '9995 kr',
+    PurchaseDate: '16-11-23',
+  },
+  {
+    ItemId: '04',
+    ItemTitle: '15-tums MacBook Pro',
+    Goal: '100',
+    Usage: '20 ggr',
+    PurchasePrice: '32995 kr',
+    PurchaseDate: '18-01-02',
+  },
+];
+
+export default DummyData;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-export { default as Input } from './Input/Input';
-export { default as Button } from './Button/Button';
-export { default as Card } from './Card/Card';
-export { default as App } from './App/App';
-export { default as LinkList } from './LinkList/LinkList';
-export { default as GlobalNav } from './GlobalNav/GlobalNav';
-export { default as Progress } from './Progress/Progress';
+export { default as Input } from "./Input/Input";
+export { default as Button } from "./Button/Button";
+export { default as Card } from "./Card/Card";
+export { default as App } from "./App/App";
+export { default as LinkList } from "./LinkList/LinkList";
+export { default as GlobalNav } from "./GlobalNav/GlobalNav";
+export { default as Progress } from "./Progress/Progress";
+export { default as DummyData } from "../DummyData";
