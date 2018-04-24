187c8e301ce7a6d48a1bd7e85a05734bff68ceea
Anna Ross <anna.ross@chasacademy.se>
2018-04-24 12:16:41 +0200

    Fix incorrect export of ProgressBar

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ export { default as Card } from './Card/Card';
 export { default as App } from './App/App';
 export { default as LinkList } from './LinkList/LinkList';
 export { default as GlobalNav } from './GlobalNav/GlobalNav';
-export { default as Progress } from './Progress/Progress';
+export { default as ProgressBar } from './ProgressBar/ProgressBar';
 export { default as Item } from './Item/Item';
 export { default as Icon } from './Icon/Icon';
 export { default as DummyData } from '../DummyData';
