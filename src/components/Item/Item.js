d817a2a061e517123ab8c3e9f53d4281b0ed13b3
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-04-25 21:26:08 +0200

    changed to class and made it statefull. The card now open and closes.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,43 +1,57 @@
 // @flow
-import * as React from 'react';
+import React, { Component } from 'react';
 import './Item.css';
 import ProgressBar from '../ProgressBar/ProgressBar';
 
+class Item extends Component {
+  constructor(props) {
+    super(props);
 
-const Item = ({ item }) => (
-  
-this.state = { isOpen: false } ,
+    // This binding is necessary to make `this` work in the callback
+    this.toggleCard = this.toggleCard.bind(this);
 
+    this.state = { isOpen: false };
+  }
 
-toggleCard = () => {
-  this.setState({
-    isOpen: !this.state.isOpen
-  });
+  toggleCard = () => {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen,
+    }));
+  };
 
+  render() {
+    const { item } = this.props;
 
-<article id={item.id} onClick={this.toggleCard}>
-<h2>{item.name}</h2>
-<p>
-  Framsteg<br />
-  <ProgressBar progressBarMax={100} progressBarCurrent={5} />
-</p>
-</article>
-
-<article id={item.id} show={this.state.isOpen} onClose={this.toggleCard}>
-    <h2>{item.name}</h2>
-    <dl>
-      <dt>Mål:</dt>
-      <dd>{item.goal}</dd>
-      <dt>Användningar:</dt>
-      <dd>{item.delimiter}</dd>
-      <dt>Inköpspris:</dt>
-      <dd>{item.price}</dd>
-    </dl>
-    <p>
-      Framsteg<br />
-      <ProgressBar progressBarMax={100} progressBarCurrent={5} />
-    </p>
-  </article>
-);
+    return (
+      <div onClick={this.toggleCard}>
+        {!this.state.isOpen ? (
+          <article id={item.id}>
+            <h2>{item.name}</h2>
+            <p>
+              Framsteg<br />
+              <ProgressBar progressBarMax={100} progressBarCurrent={5} />
+            </p>
+          </article>
+        ) : (
+          <article id={item.id}>
+            <h2>{item.name}</h2>
+            <dl>
+              <dt>Mål:</dt>
+              <dd>{item.goal}</dd>
+              <dt>Användningar:</dt>
+              <dd>{item.delimiter}</dd>
+              <dt>Inköpspris:</dt>
+              <dd>{item.price}</dd>
+            </dl>
+            <p>
+              Framsteg<br />
+              <ProgressBar progressBarMax={100} progressBarCurrent={5} />
+            </p>
+          </article>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Item;
