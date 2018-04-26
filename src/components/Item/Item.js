f06bdd3ed833fed67844dc43277ba59f7ad4ab34
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-04-26 11:03:47 +0200

    restructured the code to make it less

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,7 @@
 // @flow
-import React, { Component } from 'react';
-import './Item.css';
-import ProgressBar from '../ProgressBar/ProgressBar';
+import React, { Component } from "react";
+import "./Item.css";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 class Item extends Component {
   constructor(props) {
@@ -15,7 +15,7 @@ class Item extends Component {
 
   toggleCard = () => {
     this.setState(prevState => ({
-      isOpen: !prevState.isOpen,
+      isOpen: !prevState.isOpen
     }));
   };
 
@@ -23,36 +23,26 @@ class Item extends Component {
     const { item } = this.props;
 
     return (
-      <div onClick={this.toggleCard}>
-        {!this.state.isOpen ? (
-          <article id={item.id}>
-            <h2>{item.name}</h2>
-            <p>
-              Framsteg<br />
-              <ProgressBar progressBarMax={100} progressBarCurrent={5} />
-            </p>
-          </article>
-        ) : (
-          <article id={item.id}>
-            <h2>{item.name}</h2>
-            <dl>
-              <dt>Mål:</dt>
-              <dd>{item.goal}</dd>
-              <dt>Användningar:</dt>
-              <dd>{item.delimiter}</dd>
-              <dt>Inköpspris:</dt>
-              <dd>{item.price}</dd>
-            </dl>
-            <p>
-              Framsteg<br />
-              <ProgressBar
-                progressBarMax={item.goal}
-                progressBarCurrent={item.delimiter}
-              />
-            </p>
-          </article>
+      <article id={item.id} onClick={this.toggleCard}>
+        <h2>{item.name}</h2>
+        {this.state.isOpen && (
+          <dl>
+            <dt>Mål:</dt>
+            <dd>{item.goal}</dd>
+            <dt>Användningar:</dt>
+            <dd>{item.delimiter}</dd>
+            <dt>Inköpspris:</dt>
+            <dd>{item.price}</dd>
+          </dl>
         )}
-      </div>
+        <p>
+          Framsteg<br />
+          <ProgressBar
+            progressBarMax={item.goal}
+            progressBarCurrent={item.delimiter}
+          />
+        </p>
+      </article>
     );
   }
 }
