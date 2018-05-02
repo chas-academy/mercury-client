8f41d495792c42dae57ac6741c31cd9e0c3730a9
Anna Ross <anna.ross@chasacademy.se>
2018-05-02 23:39:55 +0200

    Update correct placement of dependencies & minor linting of files

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,14 +1,12 @@
 // @flow
-import React from 'react';
-import { Item } from '../';
-import './Items.css';
+import React from "react";
+import { Item } from "../";
+import "./Items.css";
 
-import type { ItemT } from '../../types';
+import type { ItemT } from "../../types";
 
 const Items = ({ items }: { items: Array<ItemT> }) => (
-  <section>
-    {items.map(item => <Item key={item.id} item={item} />)}
-  </section>
+  <section>{items.map(item => <Item key={item.itemId} item={item} />)}</section>
 );
 
 export default Items;
