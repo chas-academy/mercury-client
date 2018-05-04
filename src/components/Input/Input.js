1ee87b30c890dc4fddca41698a7085116290bfc9
martinfjant <martin@falkjohansson.se>
2018-05-04 17:35:14 +0200

    Lots of edits of log in and css

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 import './Input.css';
 
+<<<<<<< HEAD
 const Input = ({ variant = 'underlined', ...props }: { variant: string }) => (
   <input className={`input ${variant}`} {...props} />
+=======
+const Input = ({ color = 'light', ...props }: {color: string, props: any}) => (
+  <input className={`${color}`} {...props} />
+>>>>>>> Lots of edits of log in and css
 );
 
 export default Input;
--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import { requestLogin } from '../../actions/user';
 import { Input } from '../';
+import './LogInForm.css';
 
 class LogInForm extends Component {
   constructor(props) {
@@ -45,24 +46,41 @@ class LogInForm extends Component {
 
   render() {
     return (
+<<<<<<< HEAD
       <section className="login">
+=======
+      <section>
+>>>>>>> Lots of edits of log in and css
         <form onSubmit={this.onSubmit}>
           <Input
             type="email"
             placeholder="e-mail"
             value={this.state.formData.email}
             onChange={this.onEmailChange}
+<<<<<<< HEAD
             variant="underlined"
+=======
+            className="big"
+            autofocus="autofocus"
+>>>>>>> Lots of edits of log in and css
           />
           <Input
             type="password"
             placeholder="lösenord"
             value={this.state.formData.password}
             onChange={this.onPasswordChange}
+<<<<<<< HEAD
             variant="underlined"
           />
           <button type="submit">skicka</button>
           {this.props.authenticated ? 'du är inloggad' : 'du är inte inloggad'}
+=======
+            className="big"
+          />
+          <button type="submit">
+            Logga in
+          </button>
+>>>>>>> Lots of edits of log in and css
         </form>
       </section>
     );
