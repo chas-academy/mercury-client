b4ed98c8b913428bf4ac0e197527bebbff378722
ulander-a <alve.ulander@chasacademy.se>
2018-05-04 11:41:44 +0200

    add rudimentary log in-view

---
+++ src/views/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { LogInForm } from '../components';
+
+const LogIn = () => (
+  <div>
+    <p>Här testar vi inloggningen</p>
+    <LogInForm />
+  </div>
+);
+
+export default LogIn;
--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from './Home';
 export { default as AddItem } from './AddItem';
 export { default as Settings } from './Settings';
+export { default as LogIn } from './LogIn';
 export { default as PageNotFound } from './PageNotFound';
