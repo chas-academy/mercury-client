eb977e4127ca015bd256215dabe3a71d84e4603d
ulander-a <alve.ulander@chasacademy.se>
2018-05-04 14:22:07 +0200

    moved login logic from component to action

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,24 +1,20 @@
 // TODO: Make flow-compliant
-import Axios from 'axios';
-import JWT from 'jsonwebtoken';
+import Axios from "axios";
+import JWT from "jsonwebtoken";
 
-import {
-  LOGIN_START,
-  LOGIN_SUCCESS,
-  LOGIN_FAILURE,
-} from '../constants';
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from "../constants";
 
 export const requestToken = () => ({
-  type: LOGIN_START,
+  type: LOGIN_START
 });
 
 export const receiveUser = user => ({
   type: LOGIN_SUCCESS,
-  payload: user,
+  payload: user
 });
 
 export const requestFailure = () => ({
-  type: LOGIN_FAILURE,
+  type: LOGIN_FAILURE
 });
 
 const API_LOGIN_URL = process.env.REACT_APP_API_SIGN_IN_URL;
@@ -28,15 +24,15 @@ export const requestLogin = (formData: Object) => (dispatch: Dispatch) => {
   dispatch(requestToken());
 
   Axios.post(API_LOGIN_URL, { token })
-    .then((response) => {
+    .then(response => {
       console.log(response);
       const user = {
-        name: 'If this shows up somewhere, the reducer is working!',
+        name: "If this shows up somewhere, the reducer is working!"
       };
 
       dispatch(receiveUser(user));
     })
-    .catch((error) => {
+    .catch(error => {
       if (error.response && error.response.data.message) {
         console.error(error.response.data.message);
       } else {
