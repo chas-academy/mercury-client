6747e593ad142a4268c0252a4828d970ce3243fd
martinfjant <martin@falkjohansson.se>
2018-05-07 14:32:14 +0200

    missed a bit of waste

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,13 +3,8 @@ import React from 'react';
 
 import './Input.css';
 
-<<<<<<< HEAD
 const Input = ({ variant = 'underlined', ...props }: { variant: string }) => (
   <input className={`input ${variant}`} {...props} />
-=======
-const Input = ({ color = 'light', ...props }: {color: string, props: any}) => (
-  <input className={`${color}`} {...props} />
->>>>>>> 09cb7bc811e6563771ff894eaf20e43d7d116250
 );
 
 export default Input;
