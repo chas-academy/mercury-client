d36fb010aa5b820a9519082fbfba0ac0fc41f083
martinfjant <martin@falkjohansson.se>
2018-05-09 16:54:35 +0200

    forgot to do git add..

--- src/views/AddItem.js
+++ src/views/AddItem.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { CSSTransition } from 'react-transition-group';
-import { ItemNav, ItemNavInput, PageTitle } from '../components';
+import { StepBar, AddItemWizard, PageTitle } from '../components';
 
 const AddItem = () => (
   <React.Fragment>
     <PageTitle title="lägg till en pryl" />
-    <ItemNav />
-    <ItemNavInput />
+    <AddItemWizard />
   </React.Fragment>
 );
 
