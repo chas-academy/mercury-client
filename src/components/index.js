d797b3c779b88249174260b399dfff05a48d6281
martinfjant <martin@falkjohansson.se>
2018-05-14 12:09:36 +0200

    Attributions fixed, logout button is a dummy

--- src/components/index.js
+++ src/components/index.js
@@ -12,8 +12,7 @@ export { default as AddItemWizard } from './AddItemWizard/AddItemWizard';
 export { default as Icon } from './Icon/Icon';
 export { default as LogInForm } from './LogInForm/LogInForm';
 export { default as PageTitle } from './PageTitle/PageTitle';
-export {
-  default as NotificationComponent
-} from './NotificationComponent/NotificationComponent';
+export { default as NotificationComponent } from './NotificationComponent/NotificationComponent';
 export { default as Loader } from './Loader/Loader';
 export { default as LineButton } from './LineButton/LineButton';
+export { default as SettingsComponent } from './SettingsComponent/SettingsComponent';
