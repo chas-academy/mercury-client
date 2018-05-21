9beaa94400060d38f99d9925b233ee6fd1517e66
Anna Ross <anna.ross@chasacademy.se>
2018-05-21 16:42:23 +0200

    Add support for updating current cost per use

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,25 @@
 export { default as App } from './App/App';
-export { default as Input } from './Input/Input';
-export { default as Button } from './Button/Button';
-export { default as Box } from './Box/Box';
-export { default as GlobalNav } from './GlobalNav/GlobalNav';
-export { default as ProgressBar } from './ProgressBar/ProgressBar';
-export { default as Item } from './Item/Item';
-export { default as Items } from './Items/Items';
-export { default as StepBar } from './StepBar/StepBar';
-export { default as AddItemWizard } from './AddItemWizard/AddItemWizard';
-export { default as Icon } from './Icon/Icon';
-export { default as LogInForm } from './LogInForm/LogInForm';
+export { default as Header } from './Header/Header';
 export { default as PageTitle } from './PageTitle/PageTitle';
+export { default as GlobalNav } from './GlobalNav/GlobalNav';
 export {
   default as NotificationComponent
 } from './NotificationComponent/NotificationComponent';
 export { default as Loader } from './Loader/Loader';
-export { default as LineButton } from './LineButton/LineButton';
-export { default as CalculateGoal } from './CalculateGoal/CalculateGoal';
+export { default as Box } from './Box/Box';
+export { default as Item } from './Item/Item';
+export { default as Items } from './Items/Items';
+export { default as ItemDetail } from './ItemDetail/ItemDetail';
+export { default as LogInForm } from './LogInForm/LogInForm';
+export { default as LogOut } from './LogOut/LogOut';
+export { default as StepBar } from './StepBar/StepBar';
+export { default as AddItemWizard } from './AddItemWizard/AddItemWizard';
 export {
   default as SettingsComponent
 } from './SettingsComponent/SettingsComponent';
-export { default as LogOut } from './LogOut/LogOut';
-export { default as Header } from './Header/Header';
+export { default as Button } from './Button/Button';
+export { default as Input } from './Input/Input';
+export { default as ProgressBar } from './ProgressBar/ProgressBar';
+export { default as LineButton } from './LineButton/LineButton';
+export { default as Icon } from './Icon/Icon';
+export { default as CalculateGoal } from './CalculateGoal/CalculateGoal';
