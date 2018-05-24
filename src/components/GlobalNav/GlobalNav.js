65c4cbea506a6c6c66b348d313bbe5307a6a4aa7
Ludvig Lundgren <hello@ludviglundgren.se>
2018-05-24 15:27:38 +0200

    Dont show navbar when not logged in

--- src/components/GlobalNav/GlobalNav.js
+++ src/components/GlobalNav/GlobalNav.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Icon } from '..';
 import './GlobalNav.css';
 
-const GlobalNav = ({ active }: { active: string }) => (
-  <nav className="navbar">
-    <NavLink to="/" activeClassName="home" exact>
-      <Icon icon="items" />
-    </NavLink>
-    <NavLink to="/add" activeClassName="add">
-      <Icon icon="plus" />
-    </NavLink>
-    <NavLink to="/settings" activeClassName="settings">
-      <Icon icon="cog" />
-    </NavLink>
-  </nav>
-);
+class GlobalNav extends Component {
+  render() {
+    if (this.props.isAuth.user.authenticated) {
+      return (
+        <nav className="navbar">
+          <NavLink to="/" activeClassName="home" exact>
+            <Icon icon="items" />
+          </NavLink>
+          <NavLink to="/add" activeClassName="add">
+            <Icon icon="plus" />
+          </NavLink>
+          <NavLink to="/settings" activeClassName="settings">
+            <Icon icon="cog" />
+          </NavLink>
+        </nav>
+      );
+    }
+    return '';
+  }
+}
 
 export default GlobalNav;
